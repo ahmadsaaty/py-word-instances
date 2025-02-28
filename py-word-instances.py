d52677cb4759bb2fc72c4e7cdef42c14dd0b29ea
Ahmad Saaty <ahmad.o.saaty@gmail.com>
2025-02-28 11:07:03 +0200

    created loop that adds fruits to a list

---
+++ py-word-instances.py
@@ -0,0 +1,9 @@
+fruit_list= []
+fruit = input("please enter name of fruit: ")
+while fruit != "stop":
+
+    
+        fruit_list.append(fruit)
+        fruit = input("please enter name of fruit: ")
+            
+print(fruit_list)
